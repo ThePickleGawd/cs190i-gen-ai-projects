b3f6a2b4b8d759250d069f9f85020d6c192ad176
Dylan Lu <dylanelu@gmail.com>
2025-04-04 21:24:13 -0700

    YOLO model

--- YOLOv1/model.py
+++ YOLOv1/model.py
@@ -1,2 +1,92 @@
 import torch
+import torch.nn as nn
 
+import config
+
+class YOLO(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        layers = []
+
+        # Conv 1
+        layers += [
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ]
+
+        # Conv 2
+        layers += [
+            nn.Conv2d(in_channels=64, out_channels=192, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ]
+
+        # Conv 3
+        layers += [
+            nn.Conv2d(in_channels=192, out_channels=128, kernel_size=1, stride=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=1, stride=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ]
+
+        # Conv 4
+        for _ in range(4):
+            layers += [
+                nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1, stride=1),
+                nn.LeakyReLU(0.1),
+                nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+                nn.LeakyReLU(0.1),
+            ]
+        layers += [
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=1, stride=1),
+            nn.LeakyReLU(0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ]
+
+        # Conv 5
+        for _ in range(2):
+            layers += [
+                nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1, stride=1),
+                nn.LeakyReLU(0.1),
+                nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding=1),
+                nn.LeakyReLU(0.1),
+            ]
+        layers += [
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU(0.1),
+        ]
+
+        # Conv 6
+        layers += [
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.1),
+        ]
+
+        self.model = nn.Sequential(*layers)
+
+        self.depth = config.B * 5 + config.C
+        self.out = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024* config.S* config.S, 4096),
+            nn.LeakyReLU(0.1),
+            nn.Linear(4096, config.S * config.S * self.depth)
+        )
+
+    def forward(self, X):
+        X = self.model(X)
+        output = self.out(X)
+
+        return output.view(-1, config.S, config.S, self.depth)
--- YOLOv1/train.py
+++ YOLOv1/train.py
@@ -5,7 +5,13 @@ import matplotlib.pyplot as plt
 from data import VOCDataset
 from torch.utils.data import DataLoader
 
+from model import YOLO
 import config
+
+## Config
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 ## Dataset
 
@@ -17,9 +23,15 @@ def collate_fn(batch):
 
 train_dataloader = DataLoader(ds, batch_size=config.BATCH_SIZE, collate_fn=collate_fn)
 
+
 ## Train
+
+model = YOLO().to(device)
 
 for _ in range(config.EPOCHS):
     for images, targets in train_dataloader:
+        images, targets = images.to(device), targets.to(device)
         print(images.shape, targets.shape)
+        out = model(images)
+        print(out.shape)
         break

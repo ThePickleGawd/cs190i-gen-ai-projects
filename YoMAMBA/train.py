5e778c9e807e2dab2c09bbe780672a5ceb65e590
Dylan Lu <dylanelu@gmail.com>
2025-04-20 23:36:43 -0700

    almost done with train

--- YoMAMBA/train.py
+++ YoMAMBA/train.py
@@ -18,8 +18,10 @@ batch_size = 64
 weight_decay = 5e-4
 epochs = 140
 nworkers = 14 # Supposedly faster: https://chtalhaanwar.medium.com/pytorch-num-workers-a-tip-for-speedy-training-ed127d825db7
+save_last_model = True
+save_checkpoints = True
+checkpoint_interval = 10
 eval_interval = 10
-save_model = True
 
 # Select Model
 use_resnet18_backbone = True
@@ -93,17 +95,18 @@ def main():
         print("No backbone was specified")
         return 1
 
-    ckpt_path = f"checkpoints/{current_model}/yolov1.pth"
+    ckpt_dir = f"checkpoints/{current_model}/"
+    ckpt_path = f"{ckpt_dir}/yolov1.pth"
     metric_path = f"metrics/{current_model}/metrics.pth"
     
     # Load training settings and metrics
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay = weight_decay)
     loss_fn = YoloV1Loss()
 
-    train_loss_lst = []
-    train_mAP_lst = []
-    val_mAP_lst = []
-    val_loss_lst = []
+    train_loss_list = []
+    train_mAP_list = []
+    val_mAP_list = []
+    val_loss_list = []
     last_epoch = 0
 
     if os.path.exists(ckpt_path):
@@ -115,10 +118,10 @@ def main():
 
     if os.path.exists(metric_path):
         m = torch.load(metric_path)
-        train_loss_lst = m["train_losses"]
-        train_mAP_lst = m["train_mAP"]
-        val_loss_lst = m["val_losses"]
-        val_mAP_lst = m["val_mAP"]
+        train_loss_list = m["train_losses"]
+        train_mAP_list = m["train_mAP"]
+        val_loss_list = m["val_losses"]
+        val_mAP_list = m["val_mAP"]
 
     def lr_lambda(epoch):
         if epoch <= 5: return 1 + 9 * (epoch / 5)     # linearly from 1× to 10×
@@ -141,43 +144,52 @@ def main():
         val_pred_bbox, val_target_bbox = get_bboxes(val_loader, model, iou_threshold = 0.5, 
                                         threshold = 0.4)                                
         
-        # Train and Validation
+        # Train Step
         train_loss_value, train_time = train(train_loader, model, optimizer, loss_fn, epoch)
-        train_loss_lst.append(train_loss_value)
-        val_loss_value, val_time = val(val_loader, model, loss_fn, epoch)
-        val_loss_lst.append(val_loss_value)
+        train_loss_list.append(train_loss_value)
 
         print(
-            f"Epoch {epoch + last_epoch + 1} | "
+            f"Epoch {epoch + 1} | "
             f"LR: {optimizer.param_groups[0]['lr']:.2e} | "
-            f"Train Loss: {train_loss_value:.4f} ({train_time:.2f}s) | "
-            f"Val Loss: {val_loss_value:.4f} ({val_time:.2f}s)"
+            f"Train Loss: {train_loss_value:.4f} ({train_time:.2f}s)"
         )
 
-        # store mAP and average mAP
+        # Evaluate: Val loss, train mAP, val mAP
         if epoch > 0 and (epoch + 1) % eval_interval == 0:
+            val_loss_value, val_time = val(val_loader, model, loss_fn, epoch)
+            val_loss_list.append(val_loss_value)
             train_mAP_val = mAP(pred_bbox, target_bbox, iou_threshold = 0.5, boxformat="midpoints")
             val_mAP_val = mAP(val_pred_bbox, val_target_bbox, iou_threshold = 0.5, boxformat="midpoints")
-            train_mAP_lst.append(train_mAP_val.item())
-            val_mAP_lst.append(val_mAP_val.item())
-            print(f"Train mAP:{train_mAP_val}] val mAP:{val_mAP_val}]")
+            train_mAP_list.append(train_mAP_val.item())
+            val_mAP_list.append(val_mAP_val.item())
+            
+            print(
+                f"Val Loss: {val_loss_value:.4f} ({val_time:.2f}s) | "
+                f"Train mAP: {train_mAP_val:.4f} | Val mAP: {val_mAP_val:.4f}"
+            )
 
-
-        if save_model == True and ( (epoch + last_epoch + 1 ) % 2) == 0 or epoch + last_epoch == epochs - 1 :
+        if save_last_model:
             torch.save({
-                "epoch": epoch + last_epoch,
+                "epoch": epoch+1,
                 "model_state_dict": model.state_dict(),
                 "optimizer_state_dict": optimizer.state_dict()
-                }, ckpt_path)
-            print(f"Checkpoint at {epoch + last_epoch + 1} stored")
-            with open(f"results/{current_model}train_loss.txt","w") as values:
-                values.write(str(train_loss_lst))
-            with open(f"results/{current_model}train_mAP.txt","w") as values:
-                values.write(str(train_mAP_lst))
-            with open(f"results/{current_model}val_loss.txt","w") as values:
-                values.write(str(val_loss_lst))
-            with open(f"results/{current_model}val_mAP.txt","w") as values:
-                values.write(str(val_mAP_lst))
+            }, ckpt_path)
+            torch.save({
+                "train_losses": train_loss_list,
+                "train_mAP": train_mAP_list,
+                "val_losses": val_loss_list,
+                "val_mAP": val_mAP_list
+            }, metric_path)
+            print(f"Saved last model and metrics")
+
+        if save_checkpoints and (epoch + 1) % checkpoint_interval == 0:
+            torch.save({
+                "epoch": epoch+1,
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict()
+            }, os.path.join(ckpt_dir, f"epoch_{epoch+1}.pth"))
+            print(f"Checkpoint at {epoch + 1} stored")
+            
     
             
 if __name__ == "__main__":
